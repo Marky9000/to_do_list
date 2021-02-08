fabfd6672e9bf1d5b584994ee4bb0600fb06702d
Mark van Booren <markvanbooren@hotmail>
2021-02-08 22:49:04 +0100

    rebranch

--- api-client.js
+++ api-client.js
@@ -1,67 +1,43 @@
 
 const baseUrl = "https://jsonbox.io/box_d5a377da1bb14fb9510a";
 
-//load cuurent list and attach id to it 
-const listTotalUpdate = async()=>{
-  fetch(baseUrl)
-  .then(response => response.json())
-  .then(users => {
-      let
-        output = '<ul>';
-      users.forEach(function (user) {
-        output += `<li id="remove" >
-            ${user.description} 
-    <button class="deleteThis" onclick="deleteThisItemFrom2DoList()">
-    <i class="fas fa-trash" ><h3 >${user._id}</h3></i>
-    </button></li>`;
-      });
-      output += '</ul>';
-      document.getElementById("response").innerHTML = output;
-    });
-
-}   
-
-function addPost(event) {
-  event.preventDefault();
-  let title = document.getElementById('title').value;
-  const myPost = {
-    description: title,
-  }
-  fetch(baseUrl, {
+const listOfThings = async()=>{
+try{ const res =
+  await fetch(baseUrl, {method : 'GET'});
+  return await res.json();
+}
+catch(error){
+  console.log(error)} 
+}
+  
+//add post to API
+const addPost = async ()=> {
+let title = document.getElementById('title').value;
+const myPost = {
+  description: title,
+}
+try{await fetch (baseUrl, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-
+    headers: {'Content-Type': 'application/json',},
     body: JSON.stringify(myPost),
   })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+ } 
+catch(error){
+console.log(error)} 
 
-    const li = document.createElement("li");
-    li.innerHTML= ` <li id="remove" > ${title}  </li> `
-    response.append(li);
-
-    listTotalUpdate();
 }
 
-
-const deleteThisItemFrom2DoList = async () =>{
-
-let id =  event.target.innerText;
-//test for target ID:  
-console.log(id);
-
-await fetch(baseUrl +'/'+ id, {
+//delete item from API 
+const deleteItemFromList = async () =>{
+  let id =  event.target.innerText;
+try { 
+  const res = await fetch (baseUrl +'/'+ id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())   
-    .then(() => { console.log('removed'); }
-    )
-
-  listTotalUpdate();
- 
+    }})
+return await res.json();
+} 
+catch (error){
+console.log(error);} 
 };
--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-document.getElementById("addToList").addEventListener('click', listTotalUpdate);
-document.getElementById("addPostForm").addEventListener('submit', addPost);
+
+const listTotalUpdate = async()=>{
+
+const data = await listOfThings();
+
+let output = '<ul>';
+data.forEach(function (item) {
+        output += `<li id="remove">
+    ${item.description} 
+    <button class="deleteThis" onclick="deleteThisItemFrom2DoList()">
+    <i class="fas fa-trash">
+    <h3>${item._id}</h3></i>
+    </button></li>`;
+      });
+output += '</ul>';
+
+document.getElementById("response").innerHTML = output;    
+};
 
 listTotalUpdate();
 
+//create new post
+const makeNewPost = async ()=> {
+event.preventDefault();
+const data =  await addPost();
+const li = document.createElement("li");
+li.innerHTML= `<li id="remove">${title}</li>`
+response.append(li);
+listTotalUpdate();
+} 
+
+
+document.getElementById("addPostForm").addEventListener('submit', makeNewPost);
+document.getElementById("addToList").addEventListener('click', listTotalUpdate);
+
+//check items by class
 const list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
@@ -11,4 +42,13 @@ list.addEventListener('click', function (ev) {
 }, false);
 
 
+//delete item 
+const deleteThisItemFrom2DoList = async () =>{
+let id =  event.target.innerText;
+//test for target ID:  
+console.log(id);
+const removeThis = await deleteItemFromList ();
 
+//reload list 
+listTotalUpdate();
+};
